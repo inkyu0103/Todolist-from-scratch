bbb9d88101aa8221a60b479d9dbebd13f0b8f39c
Inkyu Kim <inkyu0103@gmail.com>
2022-02-26 17:34:32 +0900

    refactor:remove saga code

--- src/redux/slice/todoSlice.ts
+++ src/redux/slice/todoSlice.ts
@@ -1,115 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Todo } from "../../Interface/todo";
 
 interface TodoSliceInitialState {
-  todos: Todo[];
-  showedTodos: Todo[];
-  isLoading: boolean;
+  todoFilterState: string;
 }
 
 const initialState: TodoSliceInitialState = {
-  todos: [],
-  showedTodos: [],
-  isLoading: true,
+  todoFilterState: "getAll",
 };
 
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    getTodosRequest: (state) => {
-      state.isLoading = true;
-    },
-    getTodosSuccess: (state, action) => {
-      state.isLoading = false;
-      state.todos = action.payload.todos;
-      state.showedTodos = action.payload.todos;
-    },
-    getTodosFailure: (state, action) => {
-      state.isLoading = false;
-    },
-
-    toggleTodoRequest: (state, action) => {
-      state.isLoading = true;
-    },
-    toggleTodoSuccess: (state, action) => {
-      state.isLoading = false;
-      state.todos = action.payload.todos;
-      state.showedTodos = action.payload.todos;
-    },
-    toggleTodoFailure: (state, action) => {
-      state.isLoading = false;
-    },
-
-    // Mutation
-    addTodoRequest: (state, action) => {
-      state.isLoading = true;
-    },
-
-    addTodoSuccess: (state, action) => {
-      state.isLoading = false;
-      state.showedTodos = action.payload.todos;
-      state.todos = action.payload.todos;
-    },
-
-    addTodoFailure: (state, action) => {
-      state.isLoading = false;
-    },
-
-    deleteTodoRequest: (state, action) => {
-      state.isLoading = true;
-    },
-    deleteTodoSuccess: (state, action) => {
-      state.isLoading = false;
-      state.todos = action.payload.todos;
-      state.showedTodos = action.payload.todos;
-    },
-    deleteTodoFailure: (state, action) => {
-      state.isLoading = false;
-    },
-
-    editTodoRequest: (state, action) => {},
-    editTodoSuccess: (state, action) => {},
-    editTodoFailure: (state, action) => {},
-
-    // no request
     getAll: (state) => {
-      state.showedTodos = state.todos;
+      state.todoFilterState = "getAll";
     },
     getCompleted: (state) => {
-      state.showedTodos = state.todos.filter(
-        ({ is_completed }: any) => is_completed
-      );
+      state.todoFilterState = "getCompleted";
     },
     getUnCompleted: (state) => {
-      state.showedTodos = state.todos.filter(
-        ({ is_completed }: any) => !is_completed
-      );
+      state.todoFilterState = "getUnCompleted";
     },
   },
 });
 
 const { reducer, actions } = todoSlice;
 
-export const {
-  getAll,
-  getCompleted,
-  getTodosFailure,
-  getTodosRequest,
-  getTodosSuccess,
-  getUnCompleted,
-  toggleTodoFailure,
-  toggleTodoRequest,
-  toggleTodoSuccess,
-  addTodoFailure,
-  addTodoRequest,
-  addTodoSuccess,
-  editTodoRequest,
-  editTodoFailure,
-  editTodoSuccess,
-  deleteTodoRequest,
-  deleteTodoSuccess,
-  deleteTodoFailure,
-} = actions;
+export const { getAll, getCompleted, getUnCompleted } = actions;
 
 export default reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import { createBrowserHistory } from "history";
 import createSagaMiddleware from "@redux-saga/core";
 import { rootReducer } from "./slice/rootSlice";
-import rootSaga from "./saga/index";
 
 export const history = createBrowserHistory();
 
@@ -13,7 +12,5 @@ export const store = configureStore({
   middleware: [sagaMiddleware],
   devTools: true,
 });
-
-sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
